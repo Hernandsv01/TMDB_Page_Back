c61e3021a8e584820383727290e48976119e7203
Hernandsv01 <hernanxdsv@gmail.com>
2022-04-19 18:03:24 -0300

    Initialized project

---
+++ src/main/java/com/hernan/tmdbapi/TmdbapiApplication.java
@@ -0,0 +1,13 @@
+package com.hernan.tmdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdbapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hernan/tmdbapi/TmdbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hernan.tmdbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
